fe8e91ec9fd24149c56c2df012609471f1a36fd2
crecel <silverglittering@gmail.com>
2019-09-26 16:31:50 +0800

    update

--- src/main/java/io/daocloud/shop/admin/AdminApplication.java
+++ src/main/java/io/daocloud/shop/admin/AdminApplication.java
@@ -1,11 +1,13 @@
 package io.daocloud.shop.admin;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import io.daocloud.annotation.EnableDaocloudDiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableApolloConfig
+@EnableDaocloudDiscovery
 public class AdminApplication {
 
     public static void main(String[] args) {

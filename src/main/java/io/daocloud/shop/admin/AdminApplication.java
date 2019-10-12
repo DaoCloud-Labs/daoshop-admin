e18485737a7df8ccc5072a325bad687625115c7c
zhuohao.li <zhuohao.li@daocloud.io>
2019-10-12 16:18:19 +0800

    add sonar.

--- src/main/java/io/daocloud/shop/admin/AdminApplication.java
+++ src/main/java/io/daocloud/shop/admin/AdminApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableApolloConfig
+@EnableApolloConfig({"application","public.application"})
 @EnableDaocloudDiscovery
 public class AdminApplication {
 
